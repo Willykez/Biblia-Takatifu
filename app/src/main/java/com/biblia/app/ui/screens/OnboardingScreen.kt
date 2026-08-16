878ea46f95f2674a7bb652e07e731de97a5dae54
Willykez <willykez01@gmail.com>
2026-08-16 14:07:34 +0300

    Update 2 files, remove 22 files in app/src/main/java/com/biblia/app

--- app/src/main/java/com/biblia/app/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/com/biblia/app/ui/screens/OnboardingScreen.kt
@@ -23,7 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.background
+import androidx.compose.foundation.background
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
